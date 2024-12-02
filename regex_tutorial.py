60380c56a926cd44a8025c0ae7b0d6d07841b8c4
Edison Kardirsson <ed.kardirsson@hotmail.com>
2024-12-02 13:21:35 +0100

    Metacharacters

--- regex_tutorial.py
+++ regex_tutorial.py
@@ -9,3 +9,21 @@ print(re.search('Welcome', data), '\n')
 print(re.search('Welcome', data).group(), '\n')
 
 print(re.findall('e', data), '\n')
+
+# . Period metacharacter is a wild card. Can match any character except new line
+print(re.search('.', data).group(), '´\n')
+print(re.search('t.', data).group(), '´\n')
+print(re.findall('t.', data), '´\n')
+
+# * Asterisk metacharacter matches the preceding character zero or more times
+print(re.findall('t*o', data))
+print(re.findall('s*i', data))
+
+# + Plus metacharacter is similar to the asterisk, but it matches the preceding character one or more times.
+print(re.findall('t+o', data))
+
+# ? Question mark metacharacter matches either zero or one of the preceding character.
+print(re.findall('s?i', data))
+
+# Escape metacharacters (backslash) is used if you for example want to match a literal period or other special characters
+print(re.findall('\.', data))
